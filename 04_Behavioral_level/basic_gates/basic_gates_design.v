19af2ee812666bfa35f76d0ae958a234432b3240
FAROOQ ABDULLA <farooqabdulla6118@gmail.com>
2026-08-09 22:34:07 +0530

    Update basic_gates_design.v

--- 04_Behavioral_level/basic_gates/basic_gates_design.v
+++ 04_Behavioral_level/basic_gates/basic_gates_design.v
@@ -5,8 +5,6 @@ not_out,and_out,nand_out,or_out,nor_out,xor_out,xnor_out);
         and_out = a & b;
         nand_out = ~(a & b);
         or_out   = a | b;
-
-                                      25
         nor_out = ~(a | b);
         xor_out = a ^ b;
         xnor_out = ~(a ^ b);
