59111e124220d75418b077927b7c6fa2e0e660a9
FAROOQ ABDULLA <farooqabdulla6118@gmail.com>
2026-08-09 22:58:04 +0530

    Update sr_flipflop_testbench.v

--- 07_Flipflops/sr_flipflop/sr_flipflop_testbench.v
+++ 07_Flipflops/sr_flipflop/sr_flipflop_testbench.v
@@ -8,29 +8,23 @@ module SR_FF_TB;
 
  SR_FF uut(.clk(clk),.reset(reset),.S(S),.R(R),.q(q),.q_bar(q_bar));
 
-
-
   initial begin
   clk=0 ;
   forever #5 clk=~clk;
   end
   initial begin
 
-  reset= 0;S=0;R=0;
+  reset= 0; S=0; R=0;
   #10 reset=1;
-
-
-                                       44
- S=0;R=0;
+ S=0; R=0;
  #10
- S=0;R=1;
+ S=0; R=1;
  #10
- S=1;R=0;
+ S=1; R=0;
  #10
- S=1;R=1;
+ S=1; R=1;
 
-$display("Time=%0t,clk=%b,reset=%b,S=%b,R=%b,q=%b,q_bar=%b",$time,clk,reset,S,R
-,q,q_bar);
+   $display(" Time=%0t, clk=%b, reset=%b, S=%b, R=%b, q=%b, q_bar=%b", $time, clk, reset, S, R, q, q_bar);
  end
 
 endmodule
