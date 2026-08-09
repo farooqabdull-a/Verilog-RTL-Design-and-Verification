b46d4bfa8002ec393054ddddea775ae1454f23c1
FAROOQ ABDULLA <farooqabdulla6118@gmail.com>
2026-08-09 22:53:34 +0530

    Update sr_latch_testbench.v

--- 06_Latches/sr_latch/sr_latch_testbench.v
+++ 06_Latches/sr_latch/sr_latch_testbench.v
@@ -16,8 +16,7 @@ module SR_latch_TB;
         enable = 1; S = 1; R = 0; #10;
         enable = 1; S = 1; R = 1; #10;
         enable = 0; S = 0; R = 0; #10;
- $monitor("Time=%0t ,reset=%b,enable=%b,S=%b R=%b,Q=%b Qn=%b", $time, reset,
-enable, S, R, Q, Qn);
+        $monitor("Time=%0t ,reset=%b,enable=%b,S=%b R=%b,Q=%b Qn=%b", $time, reset, enable, S, R, Q, Qn);
         $finish;
     end
 endmodule
