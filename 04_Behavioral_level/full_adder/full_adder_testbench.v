26e69f9e0d75c84aea98975f5749d6538d2cb41a
FAROOQ ABDULLA <farooqabdulla6118@gmail.com>
2026-08-09 22:35:22 +0530

    Update full_adder_testbench.v

--- 04_Behavioral_level/full_adder/full_adder_testbench.v
+++ 04_Behavioral_level/full_adder/full_adder_testbench.v
@@ -7,9 +7,6 @@ module Full_adder_TB;
   initial begin
     a = 0; b = 0; cin = 0; #10;
     a = 0; b = 0; cin = 1; #10;
-
-
-                                      28
     a = 0; b = 1; cin = 0; #10;
     a = 0; b = 1; cin = 1; #10;
     a = 1; b = 0; cin = 0; #10;
